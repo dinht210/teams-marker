76220a6f2cf74c40e04ea2c6e7a77193220b2fd7
dinht210 <dinht210@gmail.com>
2025-09-21 01:40:32 -0400

    set up db

--- function_app.py
+++ function_app.py
@@ -1,25 +1,45 @@
 import azure.functions as func
 import logging
+from psycopg_pool import ConnectionPool
+import json
+import os
+
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
+pool = ConnectionPool(conninfo=os.getenv("POSTGRES_URL"), min_size=1, max_size=5)
+
 @app.route(route="add_marker")
 def add_marker(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    req_body = req.get_json()
+    # logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    # name = req.params.get('name')
+    # if not name:
+    #     try:
+    #         req_body = req.get_json()
+    #     except ValueError:
+    #         pass
+    #     else:
+    #         name = req_body.get('name')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+    # if name:
+    #     return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    # else:
+    #     return func.HttpResponse(
+    #          "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+    #          status_code=200
+    #     )
+
+@app.route(route="db_check", methods=["GET"])
+def db_check(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('DB check function processing a request.')
+    with pool.connection() as conn, conn.cursor() as cur:
+        cur.execute("SELECT version();")
+        version = cur.fetchone()[0]
+        cur.execute("SELECT count(*) FROM meetings")
+        meetings = cur.fetchone()[0]
+        cur.execute("SELECT count(*) FROM markers")
+        markers = cur.fetchone()[0]
+        logging.info(f"PostgreSQL version: {version}, meetings count: {meetings}, markers count: {markers}")
+    return func.HttpResponse("DB connection successful.")
